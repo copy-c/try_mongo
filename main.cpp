2a40964ecaf6b9a939ccdb6c87ff7d63394a0aaf
chao.zhao <271899311@qq.com>
2023-06-20 23:54:50 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,48 +1,55 @@
 #include <chrono>
 
+#include "nlohmann/json.hpp"
+
+#include <bsoncxx/json.hpp>
 #include <bsoncxx/builder/basic/array.hpp>
 #include <bsoncxx/builder/basic/document.hpp>
 #include <bsoncxx/builder/basic/kvp.hpp>
+#include <bsoncxx/builder/list.hpp>
 #include <bsoncxx/types.hpp>
 #include <mongocxx/client.hpp>
 #include <mongocxx/instance.hpp>
 #include <mongocxx/uri.hpp>
 
+using nlohmann::json;
 using bsoncxx::builder::basic::kvp;
 using bsoncxx::builder::basic::make_array;
 using bsoncxx::builder::basic::make_document;
 
 int main(int, char**) {
-    // The mongocxx::instance constructor and destructor initialize and shut down the driver,
-    // respectively. Therefore, a mongocxx::instance must be created before using the driver and
-    // must remain alive for as long as the driver is in use.
-    mongocxx::instance inst{};
-    mongocxx::client conn{mongocxx::uri{}};
+    try {
+        // The mongocxx::instance constructor and destructor initialize and shut down the driver,
+        // respectively. Therefore, a mongocxx::instance must be created before using the driver and
+        // must remain alive for as long as the driver is in use.
+        const auto uri = mongocxx::uri{
+                "mongodb+srv://user:name@cluster0.s5iqjku.mongodb.net/?retryWrites=true&w=majority"};
+        // Set the version of the Stable API on the client.
+        mongocxx::options::client client_options;
+        const auto api = mongocxx::options::server_api{mongocxx::options::server_api::version::k_version_1};
+        client_options.server_api_opts(api);
+        // Setup the connection and get a handle on the "admin" database.
+        mongocxx::client conn{uri, client_options};
+        mongocxx::database db = conn["admin"];
 
-    auto db = conn["test"];
+        // Ping the database.
+        const auto ping_cmd = bsoncxx::builder::basic::make_document(bsoncxx::builder::basic::kvp("ping", 1));
+        db.run_command(ping_cmd.view());
+        std::cout << "Pinged your deployment. You successfully connected to MongoDB!" << std::endl;
+//    auto db = client["test"];
+    } catch (const std::exception& e)
+    {
+        // Handle errors
+        std::cout<< "Exception: " << e.what() << std::endl;
+    }
+
 
     // TODO: fix dates
 
-    bsoncxx::document::value restaurant_doc = make_document(
-            kvp("address",
-                make_document(kvp("street", "2 Avenue"),
-                              kvp("zipcode", 10075),
-                              kvp("building", "1480"),
-                              kvp("coord", make_array(-73.9557413, 40.7720266)))),
-            kvp("borough", "Manhattan"),
-            kvp("cuisine", "Italian"),
-            kvp("grades",
-                make_array(
-                        make_document(kvp("date", bsoncxx::types::b_date{std::chrono::milliseconds{12323}}),
-                                      kvp("grade", "A"),
-                                      kvp("score", 11)),
-                        make_document(
-                                kvp("date", bsoncxx::types::b_date{std::chrono::milliseconds{121212}}),
-                                kvp("grade", "B"),
-                                kvp("score", 17)))),
-            kvp("name", "Vella"),
-            kvp("restaurant_id", "41704620"));
+    json test_value = R"({"key": "value"})"_json;
+    auto doc_value = bsoncxx::from_json(test_value.dump());
 
+    int i = 0;
     // We choose to move in our document here, which transfers ownership to insert_one()
-    auto res = db["restaurants"].insert_one(std::move(restaurant_doc));
+    // auto res = db["restaurants"].insert_one(std::move(bson));
 }
---
+++ storage/mongo.h
@@ -0,0 +1,8 @@
+//
+// Created by 赵超 on 2023/2/23.
+//
+
+#ifndef TRY_MONGO_MONGO_H
+#define TRY_MONGO_MONGO_H
+
+#endif //TRY_MONGO_MONGO_H
